6b4a7529672102bb5c41935e472367428ef75562
FlyinDutchmanV <FlyinDutchmanV@users.noreply.github.com>
2019-02-14 16:02:56 -0400

    Attempted a solution for test2

--- src/rdna.hpp
+++ src/rdna.hpp
@@ -9,10 +9,18 @@ string randDNA(int seed,string bases,int n)
 	mt19937 eng1(seed);		//Random generator to accept seed from variable seed 
 	string seq;					//DNA sequence variable
 	
+	if (bases.size()==0)
+	{
+		seq = "";
+		return seq;
+	}
+	
+	
+	
 	int min = 0, max = bases.size();	//set min to 0, and max to the number of bases entered by the user
 	uniform_int_distribution <int> un (min, max-1);
 		
-	for (int i=0; i< n ; i++)
+	for (int i = 0; i< n ; i++)
 		seq += bases[un(eng1)];
 	
 	return seq;				//DNA sequence returned as string
