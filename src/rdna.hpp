f313e6471a8a364174115eec402302dde5ad18e8
FlyinDutchmanV <FlyinDutchmanV@users.noreply.github.com>
2019-02-14 15:35:20 -0400

    Created function for random sequence generation

--- src/rdna.hpp
+++ src/rdna.hpp
@@ -4,16 +4,16 @@
 
 using namespace std;
 
-string randDNA(seed,bases,int n)
+string randDNA(int seed,string bases,int n)
 {
-	mt19937 eng1(seed());		//Random generator to accept seed from variable seed 
+	mt19937 eng1(seed);		//Random generator to accept seed from variable seed 
 	string seq;					//DNA sequence variable
 	
 	int min = 0, max = bases.size();	//set min to 0, and max to the number of bases entered by the user
-	uniform_int_distribution <int> un (min, max-1)
+	uniform_int_distribution <int> un (min, max-1);
 		
 	for (int i=0; i< n ; i++)
-		DNA += bases[un(eng1)];
+		seq += bases[un(eng1)];
 	
 	return seq;				//DNA sequence returned as string
 
